a75c1bde1f2a76dc8d40a550a399f241654fbdd6
Carsten Bauer <crstnbr@gmail.com>
2024-01-31 19:57:51 +0100

    improve docstring

--- src/internals.jl
+++ src/internals.jl
@@ -78,6 +78,8 @@ macro spawn(args...)
 end
 
 """
+    @spawnat thrdid expr
+
 Similar to `StableTasks.@spawn` but creates a **sticky** `Task` and schedules it to run on the thread with the given id (`thrdid`).
 The task is guaranteed to stay on this thread (it won't migrate to another thread).
 """
